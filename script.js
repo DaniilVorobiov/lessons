56d47b4076eaf5bc54d4ed2da10b3ae8937cdc6c
Daniil Vorobiov <daniilvorobiov@icloud.com>
2022-06-27 00:46:52 +0300

    урок второй

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let title = "project";
-let screens = "Easy, Hard, Interactive"; 
+let screens = "Easy, Hard, Interactive";
 let screenPrice = 100;
 let rollback = 50;
 let fullPrice = 150000;
@@ -11,15 +11,14 @@ console.log(screens.length);
 console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани");
 console.log("Стоимость разработки " + fullPrice + "рублей/ долларов/гривен/юани")
 console.log(screens.toLowerCase().split());
-console.log(fullPrice*(rollback/100));
+console.log(fullPrice * (rollback / 100));
 
 let number = 266219;
-let arrayNumber = (""+number).split("").map(Number);
+let arrayNumber = ("" + number).split("").map(Number);
 sum = 1;
-for(let i = 0;i < arrayNumber.length;i++){
+for (let i = 0; i < arrayNumber.length; i++) {
     sum *= arrayNumber[i];
 }
-sum = sum**3;
-let arraySum = (""+sum).split("").map(Number);
-console.log(arraySum[0],arraySum[1]);
-
+sum = sum ** 3;
+let arraySum = ("" + sum).split("").map(Number);
+console.log(arraySum[0] + '' + arraySum[1]);
