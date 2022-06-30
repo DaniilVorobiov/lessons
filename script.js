33a2ab4e1913f0534dd2c701b6031d84a06efb3d
Daniil Vorobiov <daniilvorobiov@icloud.com>
2022-06-30 15:47:28 +0300

    урок 4.2

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = +prompt('Сколько будет стоить данная работа?');
+let title = prompt('Как называется ваш проект?', "пРоЕкТ");
+let screens = prompt('Какие типы экранов нужно разработать?', "Простые");
+let screenPrice = +prompt('Сколько будет стоить данная работа?', 1000);
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?', "Метрика");
+let servicePrice1 = +prompt('Сколько это будет стоить?', 1000);
+let service2 = prompt('Какой дополнительный тип услуги нужен?', "Метрика");
+let servicePrice2 = +prompt('Сколько это будет стоить?', 1000);
 let rollback = 50;
 
 let allServicePrices;
 let servicePercentPrice;
-let AllServicePrices;
 let fullPrice;
 
-const getAllServicePrices = function() {
+const getAllServicePrices = function () {
     return servicePrice1 + servicePrice2;
-}
+};
+
 function getFullPrice() {
-    return screenPrice + AllServicePrices;
+    return parseInt(screenPrice) + parseInt(allServicePrices);
 }
+
 function getTitle() {
-    return title.charAt(0).toUpperCase() + title.slice(1);
+    return 
 }
+
 function getServicePercentPrices() {
-    return fullPrice - rollback;
+    return fullPrice * (rollback / 100);
 }
 
-allServicePrices = getAllServicePrices(); 
-fullPrice = getFullPrice();
-title = getTitle();
-servicePercentPrice = getServicePercentPrices();
-
-
+function getTitle(str) {
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map(function(word) {
+            return word[0].toUpperCase() + word.substr(1);
+        })
+        .join(' ');
+     }
 switch (true) {
     case fullPrice >= 30000:
         console.log("Даем скидку в 10%");
-        break; 
+        break;
     case 30000 > fullPrice > 15000:
         console.log("Даем скидку в 5%");
         break;
@@ -48,12 +52,16 @@ switch (true) {
         break;
 }
 
-console.log(typeof title);
+allServicePrices = getAllServicePrices();
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+title = getTitle(title);
+servicePercentPrice = getServicePercentPrices();
+
+console.log(title + " " + typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
-console.log(screens);
 console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
 console.log("Стоимость разработки " + fullPrice + " рублей/долларов/гривен/юани");
-console.log(screens.toLowerCase().toString().split(", "));
-console.log(fullPrice * (rollback / 100));
+console.log(screens.toLowerCase().split(", "));
 console.log(servicePercentPrice);
